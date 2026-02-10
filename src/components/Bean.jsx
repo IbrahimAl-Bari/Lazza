4d60b2f48b095e06064e1f079d19ed91987d5782
Ibrahim Al-Bari <ibrahim2010963@gmail.com>
2026-02-10 21:42:55 +0300

    final

--- src/components/Bean.jsx
+++ src/components/Bean.jsx
@@ -3,15 +3,10 @@ import React from 'react'
 const Bean = () => {
     return (
         <div className={'beans w-screen overflow-hidden h-screen flex justify-center items-center'}>
+
             <img className={"beanleft w-10 max-xs:w-5"} src="/beanleft.svg" alt=""/>
             <img className={"beanright w-10 max-xs:w-5"} src="/beanright.svg" alt=""/>
 
-            <img className={"hidden sed1 absolute"} src="/sed1.svg" alt=""/>
-            <img className={"hidden sed2 absolute"} src="/sed2.svg" alt=""/>
-            <img className={"hidden sed3 absolute"} src="/sed3.svg" alt=""/>
-            <img className={"hidden sed1 absolute"} src="/sed1.svg" alt=""/>
-            <img className={"hidden sed2 absolute"} src="/sed2.svg" alt=""/>
-            <img className={"hidden sed3 absolute"} src="/sed3.svg" alt=""/>
         </div>
     )
 }
